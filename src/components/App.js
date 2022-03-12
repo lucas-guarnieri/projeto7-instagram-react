8be5bcfdb66775251feaaeb6c426d8cb2893b558
lucas-guarnieri <lucaslguarnieri@gmail.com>
2022-03-12 12:35:02 -0300

    feat - mobile layout features implemented

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./navbar/Navbar";
 import Corpo from "./corpo/Corpo";
+import Fundomobile from "./fundomobile/Fundomobile";
 
 export default function App(){
     return(
         <div>
             <Navbar />
             <Corpo  />
+            <Fundomobile />
         </div>
     );
 }
