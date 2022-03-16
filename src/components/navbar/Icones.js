8a2fccdf684076bc8a74eb965330c824c35c6d66
Rodrigo Schmidt <rodrigodcsee@gmail.com>
2022-03-16 14:38:06 -0300

    prepara revisão

--- src/components/navbar/Icones.js
+++
@@ -1,9 +0,0 @@
-export default function Icones(){
-    const iconNames = ["paper-plane-outline", "compass-outline", "heart-outline", "person-outline"]
-    return(
-        <div class="icones">
-            {iconNames.map(icon => <ion-icon name={icon}></ion-icon>)}
-        </div>
-    );
-}
-
--- src/components/navbar/Navbar.js
+++
@@ -1,9 +0,0 @@
-import Container from "./Container";
-
-export default function Navbar(){
-    return(
-        <div class="navbar">
-            <Container />
-        </div>
-    );
-}
